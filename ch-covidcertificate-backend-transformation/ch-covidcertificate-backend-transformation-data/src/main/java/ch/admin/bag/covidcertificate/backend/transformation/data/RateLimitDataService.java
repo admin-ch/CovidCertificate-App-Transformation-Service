1807c17141f0af80979f4fbdf2aa182385341c46
Lasse_Meinen <meinen@ubique.ch>
2021-07-07 10:56:11 +0200

    Added interface for RateLimitDataService

--- ch-covidcertificate-backend-transformation/ch-covidcertificate-backend-transformation-data/src/main/java/ch/admin/bag/covidcertificate/backend/transformation/data/RateLimitDataService.java
+++ ch-covidcertificate-backend-transformation/ch-covidcertificate-backend-transformation-data/src/main/java/ch/admin/bag/covidcertificate/backend/transformation/data/RateLimitDataService.java
@@ -8,6 +8,7 @@ public interface RateLimitDataService {
      * Gets the current number of entries in the database for the given uvciHash
      *
      * @param uvciHash string for which to count the number of entries
+<<<<<<< HEAD
      * @return number of entries
      */
     public int getCurrentRate(String uvciHash);
@@ -15,7 +16,11 @@ public interface RateLimitDataService {
     /**
      * Adds an entry to the database to indicate a rate-increase
      *
+<<<<<<< HEAD
      * @param uvciHash non-empty identifier to use in the new entry
+=======
+     * @param uvciHash identifier to use in the new entry
+>>>>>>> Added interface for RateLimitDataService
      */
     public void increaseRate(String uvciHash);
 
