74cd8979403c96437598723565ad883a68564094
Lasse_Meinen <meinen@ubique.ch>
2021-07-08 17:11:34 +0200

    Added scheduling config to run cleanjob

---
+++ ch-covidcertificate-backend-transformation/ch-covidcertificate-backend-transformation-ws/src/main/java/ch/admin/bag/covidcertificate/backend/transformation/ws/config/SchedulingConfig.java
@@ -0,0 +1,33 @@
+package ch.admin.bag.covidcertificate.backend.transformation.ws.config;
+
+import ch.admin.bag.covidcertificate.backend.transformation.data.RateLimitDataService;
+import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchedulingConfig.class);
+
+    private final RateLimitDataService rateLimitDataService;
+
+    @Value("${ws.rate-limit.retention-period:PT24H")
+    private Duration retentionPeriod;
+
+    public SchedulingConfig(RateLimitDataService rateLimitDataService) {
+        this.rateLimitDataService = rateLimitDataService;
+    }
+
+    // Run cleanjob once per hour at minute 0
+    @Scheduled(cron = "${ws.rate-limit.clean-cron:0 0 * ? * *}")
+    public void cleanDB() {
+        logger.info("Cleaning rate-limit database entries");
+        rateLimitDataService.cleanDB(retentionPeriod);
+    }
+}
