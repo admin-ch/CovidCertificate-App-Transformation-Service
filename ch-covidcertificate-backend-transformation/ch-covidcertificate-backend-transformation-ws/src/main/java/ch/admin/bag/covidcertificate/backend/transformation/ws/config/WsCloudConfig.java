046a9d21c542ff8d21e74bfeb1928ae43fbd87fa
Daniel Gstoehl <gstoehl@ubiqu.ch>
2022-12-14 15:49:10 +0000

    switch from cloud connectors to cfenv

--- ch-covidcertificate-backend-transformation/ch-covidcertificate-backend-transformation-ws/src/main/java/ch/admin/bag/covidcertificate/backend/transformation/ws/config/WsCloudConfig.java
+++ ch-covidcertificate-backend-transformation/ch-covidcertificate-backend-transformation-ws/src/main/java/ch/admin/bag/covidcertificate/backend/transformation/ws/config/WsCloudConfig.java
@@ -17,9 +17,6 @@ import java.util.Map;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.CloudFactory;
-import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
-import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -49,25 +46,10 @@ public class WsCloudConfig extends WsBaseConfig {
 
     @Bean
     @Override
-    public DataSource dataSource() {
-        PoolConfig poolConfig =
-                new PoolConfig(dataSourceMaximumPoolSize, dataSourceConnectionTimeout);
-        DataSourceConfig dbConfig =
-                new DataSourceConfig(
-                        poolConfig,
-                        null,
-                        null,
-                        Map.of("leakDetectionThreshold", dataSourceLeakDetectionThreshold));
-        CloudFactory factory = new CloudFactory();
-        return factory.getCloud().getSingletonServiceConnector(DataSource.class, dbConfig);
-    }
-
-    @Bean
-    @Override
-    public Flyway flyway() {
+    public Flyway flyway(DataSource dataSource) {
         Flyway flyWay =
                 Flyway.configure()
-                        .dataSource(dataSource())
+                        .dataSource(dataSource)
                         .locations("classpath:/db/migration/pgsql_cluster")
                         .load();
         flyWay.migrate();
